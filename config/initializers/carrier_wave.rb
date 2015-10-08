a55644ae6777a829c0f547896b458ce7ed67d813
RemainZany <kredibleproperties@gmail.com>
2015-10-08 09:47:58 -0400

    Fog fixes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,25 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['s3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['s3_SECRET_KEY']
-    }
-    config.fog_directory = ENV['s3_BUCKET']
+CarrierWave.configure do |config|
+
+  # Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
+
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+
+  config.fog_credentials = {
+    # Configuration for Amazon S3
+    :provider => 'AWS',
+    :aws_access_key_id => ENV['s3_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['s3_SECRET_KEY']
+  }
+  config.fog_directory = ENV['s3_BUCKET']
   end
 end
